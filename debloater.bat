a586ef70fb7eef881a73ffc06714b9318b88a0cf
HardcodeCoder <hardcodecoder@gmail.com>
2025-11-01 16:19:23 +0530

    Fix incorrect exit code, updated output messages
    - `%errorlevel%` was not storing correct exit code for adb commands
    - Organized if else branching in both script files
    - Added comments and updated output messages

--- debloater.bat
+++ debloater.bat
@@ -8,17 +8,20 @@ echo ===========================================================================
 echo.
 
 
+:: Initialize variables and package files
 SET BLOATWARE_FILE=bloatware_list/android_google.txt
 SET DEVICE_PACKAGE_FILE=packages.pkg
 SET UNINSTALL_PACKAGE_FILE=uninstall.pkg
 
 
+:: Option for the user to determine bloatware list to use
 echo Select bloatware list to use depending on the OS:
 echo [1] Stock Android with Google Bloatware
 echo [2] MIUI/HyperOs
 set /p option= ": "
 
 
+:: Configure correct bloatware list to use based on user selection
 IF "%option%" EQU "1" (
 	SET BLOATWARE_FILE=bloatware_list/android_google.txt
 ) ELSE (
@@ -32,6 +35,7 @@ IF "%option%" EQU "1" (
 call :Println "Using bloatware file: %BLOATWARE_FILE%"
 
 
+:: Check if we have any connected devices
 call :Println "Getting Device list..."
 adb devices -l | find "device product:" > nul
 IF "%errorlevel%" EQU "1" (
@@ -40,31 +44,40 @@ IF "%errorlevel%" EQU "1" (
 )
 
 
+:: Fetch list of enabled packages from current device
 call :Println "Gathering list of installed packages..."
 adb shell pm list packages -e > %DEVICE_PACKAGE_FILE%
 
 
+:: Find bloatware packages installed in the current device
 call :Println "Preparing list of packages to uninstall..."
 call :CheckBloatwarePackages %DEVICE_PACKAGE_FILE%, %BLOATWARE_FILE%, %UNINSTALL_PACKAGE_FILE%
 
 
+:: Unintsall detected bloatware packages
 call :Println "Uninstalling packages..."
 call :UninstallPackages %UNINSTALL_PACKAGE_FILE%
-
-
 call :Println "Uninstall completed"
+
+
+:: Perform cleanup and exit
 call :Println "Performing Cleanup..."
 del %DEVICE_PACKAGE_FILE%
 del %UNINSTALL_PACKAGE_FILE%
-adb kill-server
 
 
+:: Stop adb server
+adb kill-server 1>nul 2>nul
+
+
+:: Helper to preety print headers
 :Println
 	echo %~1
 	echo.
 	exit /b 0
 	
 
+:: Utility to find bloatware packages currently installed in the device
 :CheckBloatwarePackages
 	set DEVICE_PACKAGES=%~1
     set BLOATWARE_PACKAGES=%~2
@@ -76,31 +89,32 @@ adb kill-server
 	exit /b 0
 	
 
+:: Utility to perform uninstallation of packages
 :UninstallPackages
 	SET UNINSTALL_PACKAGES=%~1
 	FOR /f "tokens=*" %%A in (%UNINSTALL_PACKAGES%) do (
 
-		echo Processing: %%A
-		echo Trying uninstall for all users
-		adb shell pm uninstall %%A > nul
+		echo Processing package: %%A
+		
+		:: Trying to uninstall package system-wide
+		adb shell pm uninstall %%A 1>nul 2>nul && (
+			echo Successfully uninstalled
+		) || (
 
-		IF %errorlevel% NEQ 0 (
-			echo Failed
-			echo Trying uninstall for current user
+			:: Trying to uninstall package for current user
+			adb shell pm uninstall --user 0 %%A 1>nul 2>nul && (
+				echo Successfully uninstalled for current user
+			) || (
 
-			adb shell pm uninstall --user 0 %%A > nul
+				:: Uninstalling failed, try to disable package
+				adb shell pm clear %%A 1>nul 2>nul
+				adb shell pm disable-user %%A 1>nul 2>nul && (
+					echo Successfully disabled for current user
+				) || (
+					echo Failed to process package
+				)
 
-			IF %errorlevel% NEQ 0 (
-				echo Failed
-				echo Trying to disable the app for current user
-				
-				adb shell pm clear %%A > nul
-				adb shell pm disable-user %%A > nul
-			) ELSE (
-				echo Success
 			)
-		) ELSE (
-			echo Success
 		)
 	)
 	exit /b 0
--- debloater.sh
+++ debloater.sh
@@ -7,11 +7,13 @@ echo "==========================================================================
 echo ""
 
 
+# Initialize variables and package files
 BLOATWARE_FILE=bloatware_list/android_google.txt
 DEVICE_PACKAGE_FILE=packages.pkg
 UNINSTALL_PACKAGE_FILE=uninstall.pkg
 
 
+# Helper to preety print headers
 function print() {
     echo ""
 	now=$(date +"%I:%M:%S")
@@ -19,11 +21,13 @@ function print() {
 }
 
 
+# Helper to check if a line is present in the given list of lines
 function exists_in_list() {
     echo "$1" | grep -F -q -x "$2"
 }
 
 
+# Utility to find bloatware packages currently installed in the device
 function check_bloatware_packages() {
     DEVICE_PACKAGES=$1
     BLOATWARE_PACKAGES=$(<$2)
@@ -39,36 +43,46 @@ function check_bloatware_packages() {
 }
 
 
+# Utility to perform uninstallation of packages
 function uninstall-packages() {
     UNINSTALL_PACKAGES=$1
+
     while read -r package <&3
     do
-        print "Processing: $package"
-        echo "Trying to uninstall for all users"
+
+        print "Processing package: $package"
+
+        # Trying to uninstall package system-wide
         adb shell pm uninstall $package > /dev/null
 
-        if [ $? -ne 0 ]; then
-            echo "Failed"
-            echo "Trying uninstall for current user"
+        if [ $? -eq 0 ]; then
+            echo "Successfully uninstalled"
+        else
 
+            # Trying to uninstall package for current user
             adb shell pm uninstall --user 0 $package > /dev/null
 
-            if [ $? -ne 0 ]; then
-                echo "Failed"
-                echo "Trying to disable the app for current user"
+            if [ $? -eq 0 ]; then
+                echo "Successfully uninstalled for current user"
+            else
 
+                # Uninstalling failed, try to disable package
                 adb shell pm clear $package > /dev/null
                 adb shell pm disable-user $package > /dev/null
-            else
-                echo "Success"
+
+                if [ $? -eq 0 ]; then
+                    echo "Successfully disabled for current user"
+                else
+                    echo "Failed to process package"
+                fi
             fi
-        else
-            echo "Success"
         fi
+
     done 3< $UNINSTALL_PACKAGES
 }
 
 
+# Option for the user to determine bloatware list to use
 echo "Select bloatware list to use depending on the OS:"
 echo "[1] Stock Android with Google Bloatware"
 echo "[2] MIUI/HyperOs"
@@ -76,6 +90,7 @@ echo -n ": "
 read option
 
 
+# Configure correct bloatware list to use based on user selection
 if [ "$option" -eq 1 ]; then
     BLOATWARE_FILE=bloatware_list/android_google.txt
 elif [ "$option" -eq 2 ]; then
@@ -87,6 +102,7 @@ fi
 print "Using bloatware file: $BLOATWARE_FILE"
 
 
+# Check if we have any connected devices
 print "Getting Device list..."
 adb devices | grep -F -w "device"
 if [ $? -ne 0 ]; then
@@ -95,21 +111,28 @@ if [ $? -ne 0 ]; then
 fi
 
 
+# Fetch list of enabled packages from current device
 print "Gathering list of installed packages..."
 adb shell pm list packages -e > $DEVICE_PACKAGE_FILE
 sed -i 's/package://' $DEVICE_PACKAGE_FILE
 
 
+# Find bloatware packages installed in the current device
 print "Preparing list of packages to uninstall..."
 check_bloatware_packages $DEVICE_PACKAGE_FILE $BLOATWARE_FILE $UNINSTALL_PACKAGE_FILE
 
 
+# Unintsall detected bloatware packages
 print "Uninstalling packages..."
 uninstall-packages $UNINSTALL_PACKAGE_FILE
-
-
 print "Uninstall completed"
+
+
+# Perform cleanup and exit
 print "Performing Cleanup..."
 rm -f $DEVICE_PACKAGE_FILE
 rm -f $UNINSTALL_PACKAGE_FILE
-adb kill-server
+
+
+# Stop adb server
+adb kill-server > /dev/null
